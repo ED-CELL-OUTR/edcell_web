b8c288843baf693e26ef326d1cd6914b93dfa929
shrutilenka <shrutilenka1202@gmail.com>
2023-05-08 07:28:17 +0530

    minor changes

--- src/components/testimonial/profile.js
+++ src/components/testimonial/profile.js
@@ -1,18 +1,18 @@
 const users = [
   {
     photo: "./../../img/image-tanya.jpg",
-    name: "Tanya Sinclair",
-    title: "UX Engineer",
+    name: ".",
+    title: ".",
     testimonial:
-      "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
+      "We are reaching out to our alumni community to gather testimonials about their experiences and achievements since graduating from our institution.",
   },
 
   {
     photo: "./../../img/image-john.jpg",
-    name: "John Tarkpor",
-    title: "Junior Front-end Developer",
+    name: ".",
+    title: ".",
     testimonial:
-      "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+      "We are reaching out to our alumni community to gather testimonials about their experiences and achievements since graduating from our institution.",
   },
 ];
 
